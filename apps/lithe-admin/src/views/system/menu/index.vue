3e0db3c61dab0ee6cb36bc6fde2d92f77de7a53f
Melonseeds <1224248739@qq.com>
2026-04-21 20:01:43 +0800

    项目工程化改造为 monorepo 结构

---
+++ apps/lithe-admin/src/views/system/menu/index.vue
@@ -0,0 +1,30 @@
+<!--
+* @description  参数1
+* @fileName  index
+* @author 刘仕强
+* @date 2026-04-21 19:34:53
+* @version V1.0.0
+!-->
+
+<template>
+  <div>菜单管理</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  defaultBoolProp: {
+    type: Boolean,
+    default: false,
+  },
+  defaultStringProp: {
+    type: String,
+    default: '',
+  },
+  defaultObjectProp: {
+    type: Object,
+    default: () => {},
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
